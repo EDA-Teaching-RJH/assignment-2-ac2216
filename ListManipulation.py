0a15b4cf833bf08d9b0eb4525b12fde9e4bad207
ac2216 <ac2216@kent.ac.uk>
2024-10-27 23:57:58 +0000

    Update ListManipulation.py

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,11 @@
 ### Part Three -- your code goes here. 
+
+numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+print('list', numbers)
+numbers.sort()
+print('list v2', numbers)
+while 1 in numbers:
+    numbers.remove(1)
+numbers.append(7)
+numbers.append(8)
+print('final list', numbers)
