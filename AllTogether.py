773a023398cee2e19f5e375976039832f3022f22
ac2216 <ac2216@kent.ac.uk>
2024-10-27 23:58:24 +0000

    Update AllTogether.py

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,18 @@
 ### Part Four -- your code goes here. 
+
+import random 
+numbers = []
+for i in range(10):
+    random_number = random.randint(1, 100)
+    numbers.append(random_number)
+
+print("Original List:", numbers) 
+
+empty = 0
+while empty < len(numbers):
+    if numbers[empty] % 2 == 0:
+        numbers.pop(empty)
+    else:
+        empty += 1
+
+print('New List:', numbers)
